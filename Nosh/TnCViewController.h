ecd5042b3518460ffa75a45a7bc1a5985a931576
xscriptz <advi@advatiyas-MacBook-Pro.local>
2016-02-18 11:22:22 -0500

    New View Controller

---
+++ Nosh/TnCViewController.h
@@ -0,0 +1,13 @@
+//
+//  TnCViewController.h
+//  Nosh
+//
+//  Created by advatiya prasad on 2/18/16.
+//  Copyright © 2016 advatiya prasad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TnCViewController : UIViewController
+
+@end
---
+++ Nosh/TnCViewController.m
@@ -0,0 +1,37 @@
+//
+//  TnCViewController.m
+//  Nosh
+//
+//  Created by advatiya prasad on 2/18/16.
+//  Copyright © 2016 advatiya prasad. All rights reserved.
+//
+
+#import "TnCViewController.h"
+
+@interface TnCViewController ()
+
+@end
+
+@implementation TnCViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
